03dfb79011f77ee0075c2e81b4269305503067a2
Ambraxil <potatololhi@gmail.com>
2026-03-06 09:16:36 -0700

    lab-3

--- lab-3/app/index.tsx
+++ lab-3/app/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet
@@ -8,9 +8,15 @@ import ToDoList from './ToDoList';
 import ToDoForm from './ToDoForm';
 
 export default function App() {
+  const [tasks, setTasks] = useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog'
+  ]);
+
   return (
     <SafeAreaView style={styles.container}>
-      <ToDoList />
+      <ToDoList tasks={tasks} />
       <ToDoForm />
     </SafeAreaView>
   );
